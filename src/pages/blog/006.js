351bf7dbf6512f337557342f68b8532dd4280543
Christian Bulabula Emungu <97136008+MrSolution07@users.noreply.github.com>
2026-02-19 22:07:34 +0200

    article 6

--- src/pages/blog/006.js
+++ src/pages/blog/006.js
@@ -22,96 +22,7 @@ const Post = () => {
   return (
     <>
       <Cursor />
-      <div className="post">
-        <Loader isOpened={isOpened} duration={0.5} />
-        <Header goBackToHome={true} disableScramble={true} />
-        <main className="flex flex-col mb-10 max-w-[900px]">
-          <article class="blog-intro">
-            <header className="px-[5%] mb-4">
-              <p className="mb-3 text-[80px]">🚗</p>
-              <h3 className="about-title text-[35px] md:text-[50px] mb-5 font-black w-full leading-[60px] md:mt-0">
-                The Boring Side of Modelling Databases
-              </h3>
-              <time datetime="2015-12-02" className="text-[18px] text-[#bbb]">
-                December 02, 2015
-              </time>
-            </header>
-            <br />
-            <br />
-            <div className="text-[var(--color-total)] max-w-full w-full mb-16 px-[5%] leading-[50px] text-[26px]">
-              <section class="tldr text-[25px] leading-[45px]">
-                <p className="mb-4">
-                  <strong>TL;DR</strong>
-                </p>
-
-                <ul className="list-disc pl-8">
-                  <li className="mb-4">
-                    
-                  </li>
-                  <li className="mb-4">
-                   
-                  </li>
-                  <li className="mb-4">
-                    
-                  </li>
-                  <li className="mb-4">
-                   
-                  </li>
-                </ul>
-                <br />
-              </section>
-              <section class="intro-text">
-                <p className="mb-4">
-                 
-                </p>
-                <p>
-                 
-                </p>
-                <br />
-                <p>
-                </p>
-                <br />
-                
-                <br />
-                <p>
-                 
-                </p>
-                <br />
-                <p>She advised us to add more stuff:</p>
-                
-                <br />
-                <p>
-                 
-                </p>
-                <br />
-                <p>
-                 
-                </p>
-                <br />
-                <p>
-                  
-                </p>
-                <br />
-                <p>
-                 
-                </p>
-                <br />
-                <p></p>
-              </section>
-            </div>
-            <br />
-            <br />
-
-            <a href={"/blog/"} className="px-[5%] text-[16px]">
-              {"<- "} Back to blog
-            </a>
-            <br />
-            <br />
-            <br />
-          </article>
-        </main>
-        <Note />
-      </div>
+     
     </>
   );
 };
@@ -119,5 +30,5 @@ const Post = () => {
 export default Post;
 
 export const Head = () => (
-  <title>006. The Boring Side of Modelling Databases | MrSolulu</title>
+  <title>006. to be dermided | MrSolulu</title>
 );
